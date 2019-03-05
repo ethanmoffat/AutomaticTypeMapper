4b1a4f065cc6e9dabd14600b93f3aa7b5c87b292
Ethan Moffat <ethanmoffat@hotmail.com>
2019-03-04 16:56:52 -0800

    Update settings for easier CI

--- Codependent/Properties/AssemblyInfo.cs
+++ Codependent/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Codependent")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Ethan Moffat 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
