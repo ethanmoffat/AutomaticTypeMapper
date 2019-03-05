c2656557a2f9ef1c6db8ace70362203d6d7ce638
Ethan Moffat <ethanmoffat@hotmail.com>
2019-03-04 17:15:48 -0800

    Add tests for basic class registration

---
+++ Codependent.Test/Types/BasicClass.cs
@@ -0,0 +1,14 @@
+﻿namespace Codependent.Test.Types
+{
+    [CodependentType()]
+    public class BasicClass { }
+
+    [CodependentType(tag: nameof(BasicClassTagged), baseType: null, singleton: false)]
+    public class BasicClassTagged { }
+
+    [CodependentType(singleton: true, baseType: null)]
+    public class BasicClassSingleton { }
+
+    [CodependentType(singleton: true, tag: nameof(BasicClassTaggedSingleton), baseType: null)]
+    public class BasicClassTaggedSingleton { }
+}
--- Codependent.Test/UnityRegistryTest.cs
+++ Codependent.Test/UnityRegistryTest.cs
@@ -1,13 +1,73 @@
-﻿using NUnit.Framework;
+﻿using Codependent.Test.Types;
+using NUnit.Framework;
+using System.Reflection;
 
 namespace Codependent.Test
 {
     [TestFixture]
     public class UnityRegistryTest
     {
-        [Test]
-        public void SomeTest()
+        public class BasicClassTests
         {
+            private static ITypeRegistry _registry;
+
+            [SetUp]
+            public void Setup()
+            {
+                var assemblyName = Assembly.GetExecutingAssembly().FullName;
+                _registry = new UnityRegistry(assemblyName);
+                _registry.RegisterDiscoveredTypes();
+            }
+
+            [Test]
+            public void TaggedClass_NoParameters_IsRegistered()
+            {
+                var instance = _registry.Resolve<BasicClass>();
+                var instance2 = _registry.Resolve<BasicClass>();
+
+                Assert.IsNotNull(instance);
+                Assert.IsNotNull(instance2);
+                Assert.AreNotSame(instance, instance2); //not singleton
+            }
+
+            [Test]
+            public void TaggedClass_WithTag_IsRegistered()
+            {
+                var instance = _registry.Resolve<BasicClassTagged>(nameof(BasicClassTagged));
+                var instance2 = _registry.Resolve<BasicClassTagged>(nameof(BasicClassTagged));
+
+                Assert.IsNotNull(instance);
+                Assert.IsNotNull(instance2);
+                Assert.AreNotSame(instance, instance2); //not singleton
+            }
+
+            [Test]
+            public void TaggedClass_Singleton_IsRegistered()
+            {
+                var instance = _registry.Resolve<BasicClassSingleton>();
+                var instance2 = _registry.Resolve<BasicClassSingleton>();
+
+                Assert.IsNotNull(instance);
+                Assert.IsNotNull(instance2);
+                Assert.AreSame(instance, instance2); //singleton
+            }
+
+            [Test]
+            public void TaggedClass_SingletonWithTag_IsRegistered()
+            {
+                var instance = _registry.Resolve<BasicClassTaggedSingleton>(nameof(BasicClassTaggedSingleton));
+                var instance2 = _registry.Resolve<BasicClassTaggedSingleton>(nameof(BasicClassTaggedSingleton));
+
+                Assert.IsNotNull(instance);
+                Assert.IsNotNull(instance2);
+                Assert.AreSame(instance, instance2); //singleton
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                //todo: dispose the container
+            }
         }
     }
 }
