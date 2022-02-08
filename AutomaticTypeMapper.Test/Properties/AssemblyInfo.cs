2a552b9b58330646741ea224c04f0e824e7089e7
Ethan Moffat <ethan@moffat.io>
2022-02-07 20:04:47 -0800

    Update version/copyrights

--- AutomaticTypeMapper.Test/Properties/AssemblyInfo.cs
+++ AutomaticTypeMapper.Test/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutomaticTypeMapper.Test")]
-[assembly: AssemblyCopyright("Copyright © Ethan Moffat 2019")]
+[assembly: AssemblyCopyright("Copyright © Ethan Moffat 2019-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
